38ea44da0f8558ccbb25d0215effc9b4dae88e5a
Will <willcheng95@gmail.com>
2018-10-23 23:24:41 +0100

    Basic view now returns minimal HTML. Corrected tearDown in functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,7 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def teardown(self):
+    def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
